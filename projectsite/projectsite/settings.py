c395a944d5ac9f99a94bdd82b7bdd69827cbe9ab
Shaaaaadork <princecharlsb@gmail.com>
2026-04-08 11:41:36 +0800

    update

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -126,7 +126,7 @@ LOGOUT_REDIRECT_URL = 'login'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 
 # --- Progressive Web App Settings ---
-PWA_APP_NAME = 'ProjectSite'
+PWA_APP_NAME = 'Hangarin'
 PWA_APP_DESCRIPTION = "A Progressive Web App version of ProjectSite"
 PWA_APP_THEME_COLOR = '#0A0A0A'
 PWA_APP_BACKGROUND_COLOR = '#FFFFFF'
